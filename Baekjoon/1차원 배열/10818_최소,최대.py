18855fdbc99112333369d1eebe02f30684dbedc9
bbo9866@naver.com <bbo9866@naver.com>
2024-02-07 22:42:39 +0900

    Day5(Basic)

---
+++ Baekjoon/1차원 배열/10818_최소,최대.py
@@ -0,0 +1,15 @@
+n = int(input())
+n_list = list(map(int, input().split()))
+print(min(n_list), max(n_list))
+# n = int(input())
+# n_list = list(map(int, input().split()))
+# max = n_list[0]
+# min = n_list[0]
+
+# for i in n_list[1:]:
+#     if i > max:
+#         max = i
+#     elif i < min:
+#         min = i
+
+# print(min, max)
